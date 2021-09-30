5a9e2e9848599234493aa3e38da5d7548b5377a0
Yonas Mulyadi <ymul0001@student.monash.edu>
2021-10-01 00:07:41 +0700

    enable cors

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const PORT = process.env.PORT;
 //configure express dependencies
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 const CredentialRouter = require('./src/routes/CredentialRoute');
 const NoteRouter = require('./src/routes/NoteRoute');
