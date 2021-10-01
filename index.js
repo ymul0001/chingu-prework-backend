6c0d9ac95959ac0fc6138c12e461392dd7f989ff
Yonas Mulyadi <ymul0001@student.monash.edu>
2021-10-01 15:57:44 +0700

    resolve cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, application/json');
+    res.header("Access-Control-Allow-Headers", '*');
     next();
 });
 
