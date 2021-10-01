612455bade4ad085ba58486880c143e0f62276d2
Yonas Mulyadi <ymul0001@student.monash.edu>
2021-10-01 23:58:07 +0700

    resolve errors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'https://notes-chingu-app.herokuapp.com'
 }));
-
+app.options('*', cors());
 const CredentialRouter = require('./src/routes/CredentialRoute');
 const NoteRouter = require('./src/routes/NoteRoute');
 
