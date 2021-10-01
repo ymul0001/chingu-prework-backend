8ba8946a594212742e331ff9e0c1be34f2798d64
Yonas Mulyadi <ymul0001@student.monash.edu>
2021-10-01 21:55:54 +0700

    resolve cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const env = require('dotenv');
+const cors = require('cors');
 env.config();
 
 const express = require('express');
@@ -8,14 +9,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
+
+
 //configure cors
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'X-Requested-With,content-type,*');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'X-Requested-With,content-type,*');
+//     next();
+// });
+
+app.use(cors());
 
 //configure express dependencies
 app.use(express.json()); 
